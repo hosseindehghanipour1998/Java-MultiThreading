d9ca5a369176d77d1ac8e5202f7a1aed28964147
Hossein <hossein.dehghanipour1998@gmail.com>
2019-11-05 18:13:40 +0330

    Session 4

--- Session 4/src/Utilities/ThreadObject.java
+++
@@ -1,7 +0,0 @@
-package Utilities;
-
-import java.util.ArrayList;
-
-public abstract class ThreadObject extends Thread{
-    public static  ArrayList<ThreadObject> getThreadPool(){return null ;};
-}
--- Session 4/src/com/company/Dehghanipour/Hossein/Main.java
+++
@@ -1,8 +0,0 @@
-package com.company.Dehghanipour.Hossein;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}

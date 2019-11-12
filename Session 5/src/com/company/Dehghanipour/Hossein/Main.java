7896541949a2ff85872eae2ce995ca28fd6b7323
Hossein <hossein.dehghanipour1998@gmail.com>
2019-11-12 19:18:10 +0330

    Fixed Bugs. The Code Works Correctly. No Need for lab report.

--- Session 5/src/com/company/Dehghanipour/Hossein/Main.java
+++ Session 5/src/com/company/Dehghanipour/Hossein/Main.java
@@ -6,12 +6,24 @@ import java.util.concurrent.locks.ReentrantLock ;
 public class Main {
 
 
-    public static ReentrantLock[] spoons = new ReentrantLock[5];
+    public static ArrayList<ReentrantLock> spoons = new ArrayList<>();
+    private static final int NUMBER_OF_PHLIOSOPHERS = 5 ;
+    private static final int NUMBER_OF_SPOONS = 5 ;
     public static void main(String[] args) {
 
+        Philosopher.initializeThreadPool(NUMBER_OF_PHLIOSOPHERS);
+        Main.initializeSpoons();
 
+        for ( Philosopher p : Philosopher.threadPool){
+            p.start();
+        }
 
-	// write your code here
+    }
+
+    private static void initializeSpoons(){
+        for(int i = 0 ; i < NUMBER_OF_SPOONS ; i++){
+            spoons.add(new ReentrantLock());
+        }
     }
 
 }
--- Session 5/src/com/company/Dehghanipour/Hossein/Philosopher.java
+++ Session 5/src/com/company/Dehghanipour/Hossein/Philosopher.java
@@ -1,47 +1,66 @@
 package com.company.Dehghanipour.Hossein;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Philosopher extends Thread {
 
-    public static ArrayList<Philosopher> threadPool = new ArrayList<>() ;
-    private static int idCounter = 1 ;
-    private int id ;
+    public static ArrayList<Philosopher> threadPool = new ArrayList<>();
+    private static int idCounter = 1;
+    private int id;
 
 
-    public static void initializeThreadPool(int threadPoolSize){
+    public static void initializeThreadPool(int threadPoolSize) {
 
-        for ( int i = 0 ; i < threadPoolSize ; i++){
+        for (int i = 0; i < threadPoolSize; i++) {
             threadPool.add(new Philosopher(idCounter++));
         }
 
     }
 
-    public Philosopher(int newId){
-        this.id = newId ;
+    public Philosopher(int newId) {
+        this.id = newId;
     }
+
+    private static int loop = 10;
 
     @Override
     public void run() {
-        super.run();
-        int spoon1 = id - 1 ;
-        int spoon2 = id ;
-        if ( Main.spoons[spoon1].tryLock()){
-            if( Main.spoons[spoon2].tryLock()) {
-                System.out.println("ID : " + id + " | Is eating.");
-                Main.spoons[spoon1].unlock();
-                Main.spoons[spoon2].unlock();
-
-                try {
-                    Thread.sleep(10);
-                }catch (Exception e){
-
-                }
+        while (loop >= 0) {
+            System.out.println("ID : " + id + " | Sleeping");
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+
+            int spoon1 = id - 1;
+            int spoon2 = id;
+            if (id == 5) {
+                spoon2 = 0;
+            }
+            if (Main.spoons.get(spoon1).tryLock()) {
+                if (Main.spoons.get(spoon2).tryLock()) {
+                    System.out.println("ID : " + id + " | Is eating.");
+                    try {
+                        Thread.sleep(200);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Main.spoons.get(spoon1).unlock();
+                    Main.spoons.get(spoon2).unlock();
+                } else {
+                    System.out.println("ID : " + id + " | Unsuccessful Attempt to eat.");
+                    Main.spoons.get(spoon1).unlock();
+                }
+
+            } else {
+                System.out.println("ID : " + id + " | Unsuccessful Attempt to eat.");
+            }
+            loop--;
+
         }
-        else{
-            Main.spoons[spoon1].unlock();
-            System.out.println("ID : " + id + " | Unsuccessful Attempt to eat.");
-        }
+
+
     }
 }
